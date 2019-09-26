b629bb91d94ff55a6c345ad1170aeaf66ad2107a
JAdrianPZ <45021900+JAdrianPZ@users.noreply.github.com>
2019-09-26 12:57:05 -0500

    Create Practica_2.sh

---
+++ Practica_2.sh
@@ -0,0 +1,18 @@
+
+#!/bin/bash
+
+clear
+echo "==============================="
+echo "Enviroment Variables"
+echo "==============================="
+echo "User: $USER"
+echo "-------------------------------"   #Llamo las variables de ambiente, no es necesario declaralas por que
+echo "Home : $HOME"                      #el sitema ya las conoce
+echo "-------------------------------"
+echo "HistControl: $HISTCONTROL"
+echo "-------------------------------"
+echo "Term : $TERM"
+echo "==============================="
+
+
+
