925a1332502c8a11fad733bff58eff80ba3fd071
JAdrianPZ <45021900+JAdrianPZ@users.noreply.github.com>
2019-09-26 12:55:58 -0500

    Create Practica_1.sh

---
+++ Practica_1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+MYVARIABLE="Esto es una variable" #Declaramos y le damos valor a las variables
+MYVARIABLE2="Esto es otra variable"
+
+echo "-----------------------------"
+echo "$MYVARIABLE"                   #Hago referencia a las varables que cree antes con el simbolo $
+echo "$MYVARIABLE2"
+echo "-----------------------------"
+
+
+
