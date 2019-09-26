87e8ae35fa5e10cfdab7ca307186f80dfe8c1e56
JAdrianPZ <45021900+JAdrianPZ@users.noreply.github.com>
2019-09-26 12:58:22 -0500

    Create Practica_3.sh

---
+++ Practica_3.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+MYUSERNAME="Adrian"
+MYPASSWORD="Pa$$W0rd"
+STARTOFSCRIPT=`date`
+echo "===================================="
+echo "Inicio de Script : $STARTOFSCRIPT"
+echo "-----------------------------------"
+echo "Este es el MYUSERNAME: $MYUSERNAME"
+echo "Estoe es MYPASSWORD: $MYPASSWORD"
+echo "-----------------------------------"
+ENDOFSCRIPT=`date`
+echo "Fin Script : $ENDOFSCRIPT"
+echo "==================================="
